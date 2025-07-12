d99bfb7df2082fcce902c09970f711c0dc5e0f3e
ThalesFortes <thales_pro@outlook.com>
2025-07-12 16:35:23 -0300

    Adjust dates

---
+++ src/modules/appointments/view.js
@@ -0,0 +1,13 @@
+import { serviceHours } from "../../utils/service-hours";
+
+const morningList = document.getElementById("morning-list")
+const afternoonList = document.getElementById("afternoon-list")
+const nightList = document.getElementById("night-list")
+
+const hourList = serviceHours
+
+export function viewAppointementsList () {
+
+  
+
+}
