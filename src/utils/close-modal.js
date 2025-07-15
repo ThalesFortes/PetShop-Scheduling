0edeb0d0dde3870b718145f1d994dafe2aa19e7e
ThalesFortes <thales_pro@outlook.com>
2025-07-15 17:27:41 -0300

    Adjusting style modal

--- src/utils/close-modal.js
+++ src/utils/close-modal.js
@@ -1,14 +1,17 @@
 const modal = document.getElementById("new-pet-scheduling")
 const closeModal = document.getElementById("imageCloseModal")
+const blurModal = document.getElementById("overlay-blur")
 
 const openModal = document.getElementById("new-scheduling")
 
 closeModal.addEventListener("click", () => {
   modal.classList.add("display-none")
+  blurModal.classList.add("display-none")
 })
 
 openModal.addEventListener("click", (event) => {
   event.preventDefault()
+  blurModal.classList.remove("display-none")
   modal.classList.remove("display-none")
 })
 
